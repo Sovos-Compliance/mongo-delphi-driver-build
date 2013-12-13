3edcf9c424084971ab75684540a6cb22853ef96b
Jose Sebastian Battig <jsbattig@convey.com>
2013-12-13 00:12:00 -0600

    Added target to build delphi 2007

--- build/build.cmd
+++ build/build.cmd
@@ -1,10 +1,16 @@
+@SET FrameworkDir=C:\Windows\Microsoft.NET\Framework\
+@SET FrameworkVersion=v4.0.30319
+@SET FrameworkSDKDir=
+
+@SET PATH=%FrameworkDir%%FrameworkVersion%;%FrameworkSDKDir%;%PATH%
+
 call build_internal_vs Release x64
 call build_internal_vs Release Win32
 call build_internal_vs Debug x64
 call build_internal_vs Debug Win32
 
 
-set BDS=C:\Program Files (x86)\Embarcadero\RAD Studio\11.0\
+call "C:\Program Files (x86)\Embarcadero\RAD Studio\11.0\bin\rsvars.bat"
 call build_internal_delphixe4 ..\DelphiMongoTests.groupproj Release Win64
 call build_internal_delphixe4 ..\DelphiMongoTests.groupproj Release Win32
 call build_internal_delphixe4 ..\DelphiMongoTests.groupproj Debug Win64
@@ -14,3 +20,7 @@ call build_internal_delphixe4 ..\DelphiMongoTests.groupproj DebugOnDemand Win32
 
 copy ..\bin\win32\Debug\MongoC.dll ..\bin\Win32\DebugOnDemand\
 copy ..\bin\x64\Debug\MongoC.dll ..\bin\x64\DebugOnDemand\
+
+call "C:\Program Files\Borland\BDS\5.0\bin\rsvars.bat"
+call build_internal_delphid2007 ..\DelphiMongoTests_D2007.groupproj Debug Win32
+call build_internal_delphid2007 ..\DelphiMongoTests_D2007.groupproj Release Win32
