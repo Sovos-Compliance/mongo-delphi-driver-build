3acba88e17b16549d1f2ddbe92be9488b6730ff9
Jose Sebastian Battig <jsbattig@convey.com>
2013-12-12 22:51:23 -0600

    Added code to build mongoc libraries

--- build/build.cmd
+++ build/build.cmd
@@ -1,7 +1,16 @@
+call build_internal_vs Release x64
+call build_internal_vs Release Win32
+call build_internal_vs Debug x64
+call build_internal_vs Debug Win32
+
+
 set BDS=C:\Program Files (x86)\Embarcadero\RAD Studio\11.0\
-call build_internal ..\DelphiMongoTests.groupproj Release Win64
-call build_internal ..\DelphiMongoTests.groupproj Release Win32
-call build_internal ..\DelphiMongoTests.groupproj Debug Win64
-call build_internal ..\DelphiMongoTests.groupproj Debug Win32
-call build_internal ..\DelphiMongoTests.groupproj DebugOnDemand Win64
-call build_internal ..\DelphiMongoTests.groupproj DebugOnDemand Win32
+call build_internal_delphixe4 ..\DelphiMongoTests.groupproj Release Win64
+call build_internal_delphixe4 ..\DelphiMongoTests.groupproj Release Win32
+call build_internal_delphixe4 ..\DelphiMongoTests.groupproj Debug Win64
+call build_internal_delphixe4 ..\DelphiMongoTests.groupproj Debug Win32
+call build_internal_delphixe4 ..\DelphiMongoTests.groupproj DebugOnDemand Win64
+call build_internal_delphixe4 ..\DelphiMongoTests.groupproj DebugOnDemand Win32
+
+copy ..\bin\win32\Debug\MongoC.dll ..\bin\Win32\DebugOnDemand\
+copy ..\bin\x64\Debug\MongoC.dll ..\bin\x64\DebugOnDemand\
