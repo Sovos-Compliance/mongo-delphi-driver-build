7cbcd841a71c52946ec081c7c2b7edb5f6d1851e
Jose Sebastian Battig <jsbattig@convey.com>
2014-03-18 18:50:57 -0500

    Updated dependency of LibBson to 0.6.5

--- build/setup.cmd
+++ build/setup.cmd
@@ -1,12 +1,12 @@
 robocopy %1\user\MongoDB\v2.5.6c ..\MongoDB /ZB /X /TEE
 
-robocopy %1\user\libbson-dll ..\bin\Win32\Debug libbson_d32_v0-6-0.dll /ZB /X /TEE 
-robocopy %1\user\libbson-dll ..\bin\Win32\DebugOnDemand libbson_d32_v0-6-0.dll /ZB /X /TEE 
-robocopy %1\user\libbson-dll ..\bin\Win32\Release libbson_r32_v0-6-0.dll /ZB /X /TEE 
+robocopy %1\user\libbson-dll ..\bin\Win32\Debug libbson_d32_v0-6-5.dll /ZB /X /TEE 
+robocopy %1\user\libbson-dll ..\bin\Win32\DebugOnDemand libbson_d32_v0-6-5.dll /ZB /X /TEE 
+robocopy %1\user\libbson-dll ..\bin\Win32\Release libbson_r32_v0-6-5.dll /ZB /X /TEE 
 
-robocopy %1\user\libbson-dll ..\bin\x64\Debug libbson_d64_v0-6-0.dll /ZB /X /TEE 
-robocopy %1\user\libbson-dll ..\bin\x64\DebugOnDemand libbson_d64_v0-6-0.dll /ZB /X /TEE 
-robocopy %1\user\libbson-dll ..\bin\x64\Release libbson_r64_v0-6-0.dll /ZB /X /TEE 
+robocopy %1\user\libbson-dll ..\bin\x64\Debug libbson_d64_v0-6-5.dll /ZB /X /TEE 
+robocopy %1\user\libbson-dll ..\bin\x64\DebugOnDemand libbson_d64_v0-6-5.dll /ZB /X /TEE 
+robocopy %1\user\libbson-dll ..\bin\x64\Release libbson_r64_v0-6-5.dll /ZB /X /TEE 
 
 
 exit /B 0
