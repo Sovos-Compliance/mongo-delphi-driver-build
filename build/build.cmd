d38bce1a3a41826a1ec5e79f9007487f3acaa010
alexeyvo <alexeyvo@softwareplanet.uk.com>
2014-10-17 07:27:59 -0500

    Remove useless code from build.cmd

--- build/build.cmd
+++ build/build.cmd
@@ -12,9 +12,6 @@ call build_internal_delphixe4 ..\DelphiMongoTests.groupproj Debug Win32
 call build_internal_delphixe4 ..\DelphiMongoTests.groupproj DebugOnDemand Win64
 call build_internal_delphixe4 ..\DelphiMongoTests.groupproj DebugOnDemand Win32
 
-copy ..\bin\win32\Debug\MongoC.dll ..\bin\Win32\DebugOnDemand\
-copy ..\bin\x64\Debug\MongoC.dll ..\bin\x64\DebugOnDemand\
-
 call "C:\Program Files\Borland\BDS\5.0\bin\rsvars.bat"
 call build_internal_delphid2007 ..\DelphiMongoTests_D2007.groupproj Debug Win32
 call build_internal_delphid2007 ..\DelphiMongoTests_D2007.groupproj Release Win32
