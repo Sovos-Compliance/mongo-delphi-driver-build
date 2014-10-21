c0fe223de06fdab90e54dc24bb3d8c3780769ed5
alexeyvo <alexeyvo@softwareplanet.uk.com>
2014-10-21 02:02:56 -0500

    Use libbson 1-0-1

--- build/setup.cmd
+++ build/setup.cmd
@@ -1,12 +1,12 @@
 robocopy %1\user\MongoDB\v2.5.6c ..\MongoDB /ZB /X /TEE
 
-robocopy %1\user\libbson-dll ..\bin\Win32\Debug libbson_d32_v1-0-0.dll /ZB /X /TEE 
-robocopy %1\user\libbson-dll ..\bin\Win32\DebugOnDemand libbson_d32_v1-0-0.dll /ZB /X /TEE 
-robocopy %1\user\libbson-dll ..\bin\Win32\Release libbson_r32_v1-0-0.dll /ZB /X /TEE 
+robocopy %1\user\libbson-dll ..\bin\Win32\Debug libbson_d32_v1-0-1.dll /ZB /X /TEE 
+robocopy %1\user\libbson-dll ..\bin\Win32\DebugOnDemand libbson_d32_v1-0-1.dll /ZB /X /TEE 
+robocopy %1\user\libbson-dll ..\bin\Win32\Release libbson_r32_v1-0-1.dll /ZB /X /TEE 
 
-robocopy %1\user\libbson-dll ..\bin\x64\Debug libbson_d64_v1-0-0.dll /ZB /X /TEE 
-robocopy %1\user\libbson-dll ..\bin\x64\DebugOnDemand libbson_d64_v1-0-0.dll /ZB /X /TEE 
-robocopy %1\user\libbson-dll ..\bin\x64\Release libbson_r64_v1-0-0.dll /ZB /X /TEE 
+robocopy %1\user\libbson-dll ..\bin\x64\Debug libbson_d64_v1-0-1.dll /ZB /X /TEE 
+robocopy %1\user\libbson-dll ..\bin\x64\DebugOnDemand libbson_d64_v1-0-1.dll /ZB /X /TEE 
+robocopy %1\user\libbson-dll ..\bin\x64\Release libbson_r64_v1-0-1.dll /ZB /X /TEE 
 
 robocopy %1\user\mongoc-dll ..\bin\Win32\Debug mongoc_d32_v1-0-1.dll /ZB /X /TEE 
 robocopy %1\user\mongoc-dll ..\bin\Win32\DebugOnDemand mongoc_d32_v1-0-1.dll /ZB /X /TEE 
