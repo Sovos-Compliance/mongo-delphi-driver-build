8bbcb83004d3349b43dc237a9538c5f73b5e4043
alexeyvo <alexeyvo@softwareplanet.uk.com>
2015-01-13 02:05:07 -0600

    Use setlocal/endlocal in build.cmd

--- build/build.cmd
+++ build/build.cmd
@@ -1,9 +1,12 @@
+setlocal enabledelayedexpansion
+
 SET FrameworkDir=C:\Windows\Microsoft.NET\Framework\
 SET FrameworkVersion=v4.0.30319
 SET FrameworkSDKDir=
 SET PATH=%FrameworkDir%%FrameworkVersion%;%FrameworkSDKDir%;%PATH%
 
 call "C:\Program Files (x86)\Embarcadero\RAD Studio\11.0\bin\rsvars.bat"
+
 call build_internal_delphixe4 Release Win64
 if ERRORLEVEL 1 goto fail
 call build_internal_delphixe4 Release Win32
@@ -17,6 +20,8 @@ if ERRORLEVEL 1 goto fail
 call build_internal_delphixe4 DynamicDllLoad Win32
 if ERRORLEVEL 1 goto fail
 
+endlocal
+
 call "C:\Program Files\Borland\BDS\5.0\bin\rsvars.bat"
 :: change FrameworkDir64 to FrameworkDir for 32-bit compilation
 set FrameworkDir=C:\Windows\Microsoft.NET\Framework\
@@ -26,8 +31,13 @@ call build_internal_delphid2007 Debug
 if ERRORLEVEL 1 goto fail
 call build_internal_delphid2007 Release
 if ERRORLEVEL 1 goto fail
+
+endlocal
+
 call build_internal_delphi5
 if ERRORLEVEL 1 goto fail
+
+endlocal
 
 exit /B 0
 
